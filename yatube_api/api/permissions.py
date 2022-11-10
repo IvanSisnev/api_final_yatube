4368644d89cca45abbb4580f4419bd6ccc5e28b1
Иван Сиснёв <ivan.sisnev@gmail.com>
2022-11-10 12:48:30 +0300

    02

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -10,14 +10,10 @@ class AuthorOrReadOnly(permissions.BasePermission):
     """
     def has_permission(self, request, view):
         """Определяет возможность осуществления запроса."""
-        return (
-            request.method in permissions.SAFE_METHODS
-            or request.user.is_authenticated
-        )
+        return (request.method in permissions.SAFE_METHODS
+                or request.user.is_authenticated)
 
     def has_object_permission(self, request, view, obj):
         """Определяет права доступа к объекту."""
-        return (
-            request.method in permissions.SAFE_METHODS
-            or obj.author == request.user
-        )
+        return (request.method in permissions.SAFE_METHODS
+                or obj.author == request.user)
