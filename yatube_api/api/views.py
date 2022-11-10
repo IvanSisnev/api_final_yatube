bf723f1fd7d2f27a3af2240feb21d6b6dade764e
Иван Сиснёв <ivan.sisnev@gmail.com>
2022-11-10 13:04:59 +0300

    02.4: игра с импортами

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -5,7 +5,7 @@ from rest_framework import viewsets, filters
 from rest_framework.pagination import LimitOffsetPagination
 from django.shortcuts import get_object_or_404
 
-from ..posts.models import Post, Group, Follow, User
+from posts.models import Post, Group, Follow, User
 from .serializers import (PostSerializer, GroupSerializer,
                           CommentSerializer, FollowSerializer)
 from .permissions import AuthorOrReadOnly
