c0dd103b61fc8d64ecd86cc0900066c952fb9b72
Иван Сиснёв <ivan.sisnev@gmail.com>
2022-11-10 12:36:57 +0300

    01

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -9,15 +9,15 @@ class AuthorOrReadOnly(permissions.BasePermission):
     Кастомный пермишен класс для разграничения доступа user/author vs. anon.
     """
     def has_permission(self, request, view):
-        """Определяют возможность запроса."""
+        """Определяет возможность запроса."""
         return (
-                request.method in permissions.SAFE_METHODS
-                or request.user.is_authenticated
+            request.method in permissions.SAFE_METHODS
+            or request.user.is_authenticated
         )
 
     def has_object_permission(self, request, view, obj):
         """Определяет права доступа к объекту."""
         return (
-                request.method in permissions.SAFE_METHODS
-                or obj.author == request.user
+            request.method in permissions.SAFE_METHODS
+            or obj.author == request.user
         )
