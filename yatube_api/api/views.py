4215eeae1cdbe5ca0d738ffd155c53f63867412f
Иван Сиснёв <ivan.sisnev@gmail.com>
2022-11-11 14:59:30 +0300

    03

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -6,7 +6,7 @@ from rest_framework.pagination import LimitOffsetPagination
 from django.shortcuts import get_object_or_404
 from rest_framework import mixins
 
-from posts.models import Post, Group, Follow, User
+from posts.models import Post, Group
 from api.serializers import (PostSerializer, GroupSerializer,
                              CommentSerializer, FollowSerializer)
 from api.permissions import AuthorOrReadOnly
